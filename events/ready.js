fe8cf66ac038a4226017aaa867d90549d5cf9ab8
CatboyDark <dqrkofficial@protonmail.com>
2024-04-29 16:17:14 -0700

    join mesage 2

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,16 @@
+import { logs } from './config.json'
 const { Events, EmbedBuilder } = require('discord.js');
 
 const onlineEmbed = new EmbedBuilder()
 	.setColor(0x000000)
 	.setTitle('Bot is online!')
-	
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) 
 	{
 		console.log(`r`);
-		client.channels.cache.get('1234634679349415967').send({ embeds: [onlineEmbed] });
+		client.channels.cache.get(logs).send({ embeds: [onlineEmbed] });
 	},
 };
