5274c73419fbeb70c70ff5720fd098127ef2dcc0
CatboyDark <dqrkofficial@protonmail.com>
2024-04-29 16:12:03 -0700

    join message

--- events/ready.js
+++ events/ready.js
@@ -1,23 +1,9 @@
 const { Events, EmbedBuilder } = require('discord.js');
 
 const onlineEmbed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-	.setImage('https://i.imgur.com/AfFp7pu.png')
-	.setTimestamp()
-	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
+	.setColor(0x000000)
+	.setTitle('Bot is online!')
+	
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
