a2a0e28c6b1e247a30db13d157b649c73798faa3
CatboyDark <dqrkofficial@protonmail.com>
2024-04-29 21:44:13 -0700

    more rank commands

--- cmds/slash/role.js
+++ cmds/slash/role.js
@@ -8,7 +8,9 @@ module.exports =
 {
 	data: new SlashCommandBuilder()
 		.setName('role')
-		.setDescription('roles you!'),
+		.setDescription('Add/Remove a user\'s roles')
+		.addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
+		.addUserOption(option => option.setName('role').setDescription('Select a role').setRequired(true)),
 		
 	async execute(interaction) {
 		await interaction.reply({ embeds: [help] });
