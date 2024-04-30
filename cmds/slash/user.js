cfbb6e5659e241f88410a9e8369fe5d17985bd09
CatboyDark <dqrkofficial@protonmail.com>
2024-04-29 17:54:37 -0700

    role command

---
+++ cmds/slash/user.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = 
+{
+	data: new SlashCommandBuilder()
+		.setName('role')
+		.setDescription('Edit a user\'s roles')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
+        .addUserOption(option => option.setName('User').setDescription('')).setRequired(true)
+        .addRoleOption(option => option.setName('Role').setDescription('')).setRequired(true),
+
+	async execute(interaction) {
+		    await interaction.reply('It works!');
+	},
+};
