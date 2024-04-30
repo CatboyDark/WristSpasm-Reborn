e0277616801db0730dcf461952d76f864df767c5
CatboyDark <dqrkofficial@protonmail.com>
2024-04-30 00:07:24 -0700

    role command WORKS :thumbs-up:

--- cmds/slash/role.js
+++ cmds/slash/role.js
@@ -1,16 +1,25 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-const role = new EmbedBuilder()
-	.setColor(0x000000)
-	.setTitle('WIP')
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = 
 {
 	data: new SlashCommandBuilder()
 		.setName('role')
-		.setDescription('Add/Remove a user\'s roles'),
+		.setDescription('Add or remove a user\'s roles')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
+		.addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
+		.addRoleOption(option => option.setName('role').setDescription('Select a role').setRequired(true)),
 		
 	async execute(interaction) {
-		await interaction.reply({ embeds: [role] });
-	},
-};	
+
+		const user = interaction.options.getMember('user');
+		const role = interaction.options.getRole('role');
+
+		if (user.roles.cache.has(role.id)) { 
+			user.roles.remove(role).catch(console.error)
+			await interaction.reply({ embeds: [new EmbedBuilder().setDescription(`${user} is no longer ${role}`).setColor('#FF0000')] });
+		} else { 
+			user.roles.add(role).catch(console.error)
+			await interaction.reply({ embeds: [new EmbedBuilder().setDescription(`${user} is now ${role}`).setColor('#00FF00')] });
+		}
+	}
+};
