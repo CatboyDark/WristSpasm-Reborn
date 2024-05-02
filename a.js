67d510e1e685c8d1c5198ce60d737dc529c2864b
CatboyDark <dqrkofficial@protonmail.com>
2024-05-01 17:53:49 -0700

    well everything works

--- a.js
+++ a.js
@@ -36,17 +36,14 @@ for (const folder of commandFolders) {
 
 // Events
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventsFolder = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+for (const file of eventsFolder) {
+  const event = require(path.join(eventsPath, file));
+  event.once
+  	? client.once(event.name, (...args) => event.execute(...args))
+  	: client.on(event.name, (...args) => event.execute(...args));
 }
 
 client.login(token);
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,11 +7,6 @@ module.exports = {
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
-
 		try {
 			await command.execute(interaction);
 		} catch (error) {
