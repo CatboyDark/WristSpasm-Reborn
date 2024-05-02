67c28e368576021ad3fbcd9c105cb92a6e1f2e0d
CatboyDark <dqrkofficial@protonmail.com>
2024-05-02 03:27:41 -0700

    updated help.js todo: make staff commands private button

--- cmds/slash/help.js
+++ cmds/slash/help.js
@@ -1,16 +1,35 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
-const help = new EmbedBuilder()
-	.setColor(0x000000)
-	.setTitle('WIP')
-
 module.exports = 
 {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Helps you!'),
+		.setDescription('Shows bot info'),
 		
 	async execute(interaction) {
-		await interaction.reply({ embeds: [help] });
+
+		const commands = await interaction.client.application.commands.fetch();
+		const commandList = commands
+		.sort((a, b) => a.name.localeCompare(b.name))
+		.map(cmd => `* **\`\/${cmd.name}\`** ${cmd.description}`)
+		.join('\n');
+
+		const info = new EmbedBuilder()
+			.setColor(0x000000)
+			.setThumbnail('https://i.imgur.com/uwqAaeb.png')
+			.setTitle('The Official WristSpasm Bot!')
+			.addFields(
+				{ name: '**Commands**', value: `${commandList}` },
+				{ name: '**✦ Credits ✦**', value: 
+					`<@1165302964093722697>
+					<@486155512568741900>
+					<@1169174913832202306>`}
+			)
+			.setFooter({
+				text: "Created by @CatboyDark",
+				iconURL: "https://i.imgur.com/4lpd01s.png"
+			})
+
+		await interaction.reply({ embeds: [info] });
 	},
 };
