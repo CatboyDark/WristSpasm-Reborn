499c3b1e3951a944165c8bfe1ce574ee3e117087
CatboyDark <dqrkofficial@protonmail.com>
2024-05-08 12:37:25 -0700

    cleaned up folder detection DCinit.js

--- src/discord/DCinit.js
+++ src/discord/DCinit.js
@@ -19,27 +19,36 @@ class DCinit
 		this.token = token;
         this.client.commands = new Collection();
 
-		const foldersPath = path.join(__dirname, 'cmds');
-        const commandFolders = fs.readdirSync(foldersPath);
+		// Commands
 
-        for(const folder of commandFolders) {
-            const commandsPath = path.join(foldersPath, folder);
-            const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-            for (const file of commandFiles) {
-                const filePath = path.join(commandsPath, file);
-                const command = require(filePath);
-                this.client.commands.set(command.data.name, command);
-            }
+		const commandsPath = path.join(__dirname, 'cmds');
+        const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+        for(const file of commandFiles) {
+            const command = require(path.join(commandsPath, file));
+            this.client.commands.set(command.data.name, command);
         }
 
-		const eventsPath = path.join(__dirname, 'events');
-        const eventsFolder = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+		// Events
 
-        for(const file of eventsFolder) {
+		const eventsPath = path.join(__dirname, 'events');
+        const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+
+        for(const file of eventFiles) {
             const event = require(path.join(eventsPath, file));
             event.once
                 ? this.client.once(event.name, (...args) => event.execute(...args))
                 : this.client.on(event.name, (...args) => event.execute(...args));
+        }
+
+		// Features
+
+		const featuresPath = path.join(__dirname, 'features');
+        const featureFiles = fs.readdirSync(featuresPath).filter(file => file.endsWith('.js'));
+
+        for (const file of featureFiles) {
+            const feature = require(path.join(featuresPath, file));
+            feature(this.client);
         }
     }
 
