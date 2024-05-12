907628d5efaf79354868646c9d78827e2a9a62e5
CatboyDark <dqrkofficial@protonmail.com>
2024-05-11 18:33:44 -0700

    everything works! boutta buy a plane ticket to au so i can kiss kathy

--- src/discord/events/interactionCreate.js
+++ src/discord/events/interactionCreate.js
@@ -33,7 +33,7 @@ module.exports = {
 					const roleId = data[interaction.message.embeds[0].description];
 					const role = interaction.guild.roles.cache.get(roleId);
 
-					if (roleId === 1183240686678589541)
+					if (roleId === '1183240686678589541')
 					{ if (!member.roles.cache.has(config.gmemberRole)) { return interaction.reply({ content: 'You must be a guild member to join our SMP!', ephemeral: true }); } }
 			
 					if (member.roles.cache.has(role.id)) {
